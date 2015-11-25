022e77ba07a1fa8aebbc1a08746ea86ead5443ac
Piotr Hosa <piotrhosa@gmail.com>
2015-11-25 19:54:07 +0000

    Changed the layout of main so that all buttons are visible.

--- Bikepool/mobile/src/main/java/uk/ac/gla/bikepool/BikePoolActivity.java
+++ Bikepool/mobile/src/main/java/uk/ac/gla/bikepool/BikePoolActivity.java
@@ -2,12 +2,28 @@ package uk.ac.gla.bikepool;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class BikePoolActivity extends AppCompatActivity{
+public class BikePoolActivity extends AppCompatActivity {
+    TextView nameTV;
+    TextView startTV;
+    TextView finishTV;
+    TextView descriptionTV;
+    TextView membersTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pool);
+
+        hookText();
+    }
+
+    private void hookText() {
+        nameTV = (TextView) findViewById(R.id.text_name);
+        startTV = (TextView) findViewById(R.id.text_start);
+        finishTV = (TextView) findViewById(R.id.text_finish);
+        descriptionTV = (TextView) findViewById(R.id.text_desc);
+        membersTV = (TextView) findViewById(R.id.text_members);
     }
 }
